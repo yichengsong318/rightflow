bcd4e03e389f9936f75cff6ea872ac6dfc56ab18
Marius SODJINOU <marius@right-com.com>
2019-12-10 20:03:50 +0100

    Add prettier configuration for project

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './vendors.css';
-import './styles.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./vendors.css";
+import "./styles.css";
 
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 serviceWorker.unregister();
