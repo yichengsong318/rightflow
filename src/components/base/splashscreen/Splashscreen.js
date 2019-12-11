2035afab508685c25720038f1643b20502ab8062
Marius SODJINOU <marius@right-com.com>
2019-12-11 14:49:04 +0100

    Add splashscreen component and stories

--- src/components/base/splashscreen/Splashscreen.js
+++ src/components/base/splashscreen/Splashscreen.js
@@ -1,10 +1,55 @@
-import React from "react";
-import "./Splashscreen.css";
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useRef, useState, useEffect } from "react";
 
-export default function Splashscreen() {
+import styled from "styled-components";
+import bgSplashscreen from "assets/images/splashscreen-background.svg";
+import AppLogo from "components/base/logo/AppLogo";
+import AppLogoIcon from "components/base/logo-icon/AppLogoIcon";
+
+const SplashscreenContainer = styled.div`
+  background: url(${bgSplashscreen}) no-repeat bottom;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+`;
+
+export default function Splashscreen(props) {
+  const hasMount = useRef(false);
+  const [delaying, setDelaying] = useState(false);
+
+  useEffect(() => {
+    if (!hasMount.current) {
+      // componentDidMount
+      if (props.static) {
+        // For render static state of this component
+        if (props.static === "delaying") {
+          setDelaying(true);
+        } else if (props.static === "fulled") {
+          setDelaying(false);
+        }
+      } else {
+        initDelay();
+      }
+      hasMount.current = true;
+    } else {
+      // componentDidUpdate
+    }
+    return () => {
+      // componentWillUnmount
+    };
+  });
+
+  const initDelay = () => {
+    setDelaying(true);
+    setTimeout(() => {
+      setDelaying(false);
+    }, 600);
+  };
+
   return (
-    <div className="w-screen h-screen">
-      <div className=""></div>
-    </div>
+    <SplashscreenContainer className="w-full h-full flex items-center justify-center">
+      {delaying ? <AppLogoIcon /> : <AppLogo />}
+    </SplashscreenContainer>
   );
 }
--- src/components/base/splashscreen/Splashscreen.stories.js
+++ src/components/base/splashscreen/Splashscreen.stories.js
@@ -2,4 +2,8 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import Splashscreen from "./Splashscreen";
 
-storiesOf("Base|Splashscreen", module).add("default", () => <Splashscreen />);
+storiesOf("Base|Splashscreen", module)
+  .addDecorator(storyFn => <div className="h-screen">{storyFn()}</div>)
+  .add("default", () => <Splashscreen />)
+  .add("delaying", () => <Splashscreen static="delaying" />)
+  .add("fulled", () => <Splashscreen static="fulled" />);
