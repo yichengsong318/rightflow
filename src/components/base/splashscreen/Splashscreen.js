f73bccb16542e5497ae6a52796846a627e2972b7
Marius SODJINOU <marius@right-com.com>
2019-12-12 15:14:50 +0100

    Add splashcreen component

--- src/components/base/splashscreen/Splashscreen.js
+++ src/components/base/splashscreen/Splashscreen.js
@@ -1,10 +1,12 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useRef, useState, useEffect } from "react";
 
+import PropTypes from "prop-types";
 import styled from "styled-components";
 import bgSplashscreen from "assets/images/splashscreen-background.svg";
 import AppLogo from "components/base/logo/AppLogo";
 import AppLogoIcon from "components/base/logo-icon/AppLogoIcon";
+// import { useMappedState } from "redux-react-hook";
 
 const SplashscreenContainer = styled.div`
   background: url(${bgSplashscreen}) no-repeat bottom;
@@ -17,6 +19,8 @@ const SplashscreenContainer = styled.div`
 export default function Splashscreen(props) {
   const hasMount = useRef(false);
   const [delaying, setDelaying] = useState(false);
+  // const mapState = useCallback(state => state.timeDelaySplashscreen, []);
+  // const delayTime = useMappedState(mapState);
 
   useEffect(() => {
     if (!hasMount.current) {
@@ -44,7 +48,7 @@ export default function Splashscreen(props) {
     setDelaying(true);
     setTimeout(() => {
       setDelaying(false);
-    }, 600);
+    }, props.delay);
   };
 
   return (
@@ -61,3 +65,12 @@ export default function Splashscreen(props) {
     </SplashscreenContainer>
   );
 }
+
+Splashscreen.propTypes = {
+  static: PropTypes.string,
+  delay: PropTypes.number
+};
+
+Splashscreen.defaultProps = {
+  delay: 600
+};
--- src/components/base/splashscreen/Splashscreen.stories.js
+++ src/components/base/splashscreen/Splashscreen.stories.js
@@ -2,8 +2,10 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import Splashscreen from "./Splashscreen";
 
+const delayTime = 600;
+
 storiesOf("Base|Splashscreen", module)
   .addDecorator(storyFn => <div className="h-screen">{storyFn()}</div>)
-  .add("default", () => <Splashscreen />)
+  .add("default", () => <Splashscreen delay={delayTime} />)
   .add("delaying", () => <Splashscreen static="delaying" />)
   .add("fulled", () => <Splashscreen static="fulled" />);
