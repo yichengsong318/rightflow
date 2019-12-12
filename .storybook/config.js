8375f37433c40a8cbfb95523137076dce146ef70
Marius SODJINOU <marius@right-com.com>
2019-12-12 15:13:08 +0100

    Register storybook knobs addon and add redux

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,18 @@
 import React from "react";
+import { StoreContext } from "redux-react-hook";
 import { configure, addParameters, addDecorator } from "@storybook/react";
-import { theme } from "./rf-theme";
-import '../src/vendors.css';
-import '../src/styles.css';
-import './stories.css';
+import { withKnobs } from "@storybook/addon-knobs/react";
+import { theme } from "./rf-theme"; // custom storybook theme
+
+// import css styles
+import "../src/vendors.css";
+import "../src/styles.css";
+import "./stories.css"; // stories global css
+
+// add redux for state management
+import { makeStore } from "../src/store";
+const store = makeStore();
+
 
 // Add global parameters
 addParameters({
@@ -12,12 +21,19 @@ addParameters({
   },
 });
 
-// Add global decorators
-addDecorator(storyFn => <div className="story-container"> {storyFn()}</div>)
 
+// Add global decorators. The order matter
+addDecorator(storyFn => (
+  <StoreContext.Provider value={store}>{storyFn()}</StoreContext.Provider>
+));
+addDecorator(withKnobs); // For Knobs addons to storybook (use to live edit component data)
+addDecorator(storyFn => <div className="story-container"> {storyFn()}</div>); // Add wrapper container for stories
+
+// Create loader
 const loaderFn = () => {
-  const req = require.context('../src/components', true, /\.stories\.js$/);
+  const req = require.context("../src/components", true, /\.stories\.js$/);
   req.keys().forEach(fname => req(fname));
 };
 
+// run configuration
 configure(loaderFn, module);
