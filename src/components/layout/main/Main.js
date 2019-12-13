ddfe5b76baa95af200845b507ecee7f5771183ea
Marius SODJINOU <marius@right-com.com>
2019-12-13 20:24:03 +0100

    Update Main component to add routing

--- src/components/layout/main/Main.js
+++ src/components/layout/main/Main.js
@@ -1,18 +1,32 @@
-import React, { useCallback, useEffect, useRef } from "react";
-import { useMappedState } from "redux-react-hook";
+import React, { useEffect, useRef, useState } from "react";
+// import { useMappedState } from "redux-react-hook";
+import { Switch, Route, Redirect } from "react-router-dom";
+import Login from "components/ui/login/Login";
+import Welcome from "components/ui/welcome/Welcome";
+import NotFound from "components/ui/not-found/NotFound";
+import Forbidden from "components/ui/forbidden/Forbidden";
+import InternalError from "components/ui/internal-error/InternalError";
+import Home from "components/layout/home/Home";
+import AppView from "components/layout/app-view/AppView";
+import Onboarding from "components/ui/onboarding/Onboarding";
+import { NAVIGATION } from "constants/constants";
 
-export default function Welcome() {
+export default function Main() {
+  const [welcomeEnded, setWelcomeEnded] = useState(false);
   const hasMount = useRef(false);
-  const mapState = useCallback(state => state.userIsConnected, []);
-  const isConnected = useMappedState(mapState);
+
+  const mainRoutes = [
+    { path: NAVIGATION.LOGIN, exact: true, component: Login },
+    { path: NAVIGATION.ONBOARDING, exact: false, component: Onboarding },
+    { path: NAVIGATION.APP, exact: false, component: AppView },
+    { path: NAVIGATION.NOT_FOUND, exact: true, component: NotFound },
+    { path: NAVIGATION.FORBIDDEN, exact: true, component: Forbidden },
+    { path: NAVIGATION.INTERNAL_ERROR, exact: true, component: InternalError }
+  ];
 
   useEffect(() => {
     if (!hasMount.current) {
       // componentDidMount
-      if (isConnected) {
-        //
-      }
-      hasMount.current = true;
     } else {
       // componentDidUpdate
     }
@@ -21,5 +35,38 @@ export default function Welcome() {
     };
   });
 
-  return <div></div>;
+  function InitializingHandler() {
+    if (!hasMount.current) {
+      hasMount.current = true;
+    }
+    return <Welcome onLoaded={() => onWelcomeEnded()} />;
+  }
+
+  function onWelcomeEnded() {
+    setWelcomeEnded(true);
+  }
+
+  return (
+    <React.Fragment>
+      {hasMount.current && welcomeEnded ? (
+        <Switch>
+          {mainRoutes.map((route, index) => (
+            <Route
+              key={index}
+              path={route.path}
+              exact={route.exact}
+              component={route.component}
+            />
+          ))}
+          <Route exact path="/" component={Home}></Route>
+          <Route
+            path="*"
+            render={({ location }) => <Redirect to={NAVIGATION.NOT_FOUND} />}
+          ></Route>
+        </Switch>
+      ) : (
+        <InitializingHandler />
+      )}
+    </React.Fragment>
+  );
 }
