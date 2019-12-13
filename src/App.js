4e2842cb01f1f860e9f363321cfb15c32d4ba104
Marius SODJINOU <marius@right-com.com>
2019-12-13 20:17:12 +0100

    Change app main route handler

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
-import Welcome from "components/ui/welcome/Welcome";
+import Main from "components/layout/main/Main";
 
 function App() {
   return (
     <Router>
-      <Welcome />
+      <Main />
     </Router>
   );
 }
