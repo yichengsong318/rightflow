5127aa169fbecb22a3d6062953e2b6e7d15a40a8
Marius SODJINOU <marius@right-com.com>
2019-12-13 20:16:52 +0100

    Create constants for app

---
+++ src/constants/constants.js
@@ -0,0 +1,11 @@
+export const NAVIGATION = {
+  HOME: "/",
+  LOGIN: "/login",
+  ONBOARDING: "/onboarding",
+  APP: "/app",
+  DASHBOARD: "/app/dashboard",
+  APP_ONBOARDING: "/app/onboarding",
+  NOT_FOUND: "/404",
+  FORBIDDEN: "/403",
+  INTERNAL_ERROR: "/500"
+};
