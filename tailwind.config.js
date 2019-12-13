a29f11c1e6089cbece4387849d503d5cff363277
Marius SODJINOU <marius@right-com.com>
2019-12-13 20:15:16 +0100

    Add config to tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,20 @@
 module.exports = {
   theme: {
+    container: {
+      center: true
+    },
+    screens: {
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+      xxl: "1440px"
+      // => @media (min-width: 1440px) { ... }
+    },
     transitionProperty: {
       // defaults to these values
       none: "none",
