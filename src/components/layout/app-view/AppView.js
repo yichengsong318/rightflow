e02c9eddf06912198f84d9426a65cf7c5fa81e48
Marius SODJINOU <marius@right-com.com>
2019-12-17 13:49:56 +0100

    Add auth to dashboard view and onboarding

--- src/components/layout/app-view/AppView.js
+++ src/components/layout/app-view/AppView.js
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useCallback, Suspense } from "react";
+import { useMappedState } from "redux-react-hook";
+import Loader from "components/base/loader/Loader";
+import { Redirect } from "react-router-dom";
+import { NAVIGATION } from "constants/constants";
 
 export default function AppView() {
+  const mapIsConnectedState = useCallback(state => state.userIsConnected, []);
+  const userIsConnected = useMappedState(mapIsConnectedState);
+
   return (
-    <div className="w-full h-full flex items-center justify-center">
-      App coming soon...
-    </div>
+    <React.Fragment>
+      {userIsConnected ? (
+        <Suspense fallback={<Loader />}>
+          <div className="w-full h-full flex items-center justify-center">
+            App coming soon...
+          </div>
+        </Suspense>
+      ) : (
+        <Redirect to={NAVIGATION.LOGIN} />
+      )}
+    </React.Fragment>
   );
 }
