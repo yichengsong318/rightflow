bda2f25771b73d26f5d6a3239fefaa1c11a6db89
Marius SODJINOU <marius@right-com.com>
2019-12-17 14:48:45 +0100

    Fix server port error

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-const port = process.env.PORT || 9005;
+const port = process.env.PORT || 9004;
 
 app.use(express.static(path.join(__dirname, "build")));
 
