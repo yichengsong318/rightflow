914c187cd1a8889dc86b24b714e9bf9d07a35d41
Marius SODJINOU <marius@right-com.com>
2019-12-17 14:48:05 +0100

    Fix server routing issue

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
 
-const port = process.env.PORT || 9004;
+const port = process.env.PORT || 9005;
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, "build")));
 
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 app.listen(port);
