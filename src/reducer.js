e6450772aba816e02dce5509236ba2c25aec47e2
Marius SODJINOU <marius@right-com.com>
2019-12-17 12:12:08 +0100

    update reducer

--- src/reducer.js
+++ src/reducer.js
@@ -4,8 +4,14 @@ export default function reducer(state, action) {
   }
 
   switch (action.type) {
-    case "UPDATE_USER_CONNECTED": {
+    case "UPDATE_USER_IS_CONNECTED": {
       return { ...state, userIsConnected: action.value };
+    }
+    case "UPDATE_USER_INFO": {
+      return { ...state, userInfo: action.value };
+    }
+    case "UPDATE_SOCKET_INSTANCE": {
+      return { ...state, socketIoInstance: action.value };
     }
     default:
       return state;
