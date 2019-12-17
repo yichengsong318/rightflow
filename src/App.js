2bcbfb2324647deec367d98a353efb4c30f0a3ca
Marius SODJINOU <marius@right-com.com>
2019-12-17 12:11:36 +0100

    Add socket io to app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { useEffect, useCallback } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import Main from "components/layout/main/Main";
+import io from "socket.io-client";
+import { SOCKET } from "constants/constants";
+import { useDispatch } from "redux-react-hook";
 
 function App() {
+  const socket = io(SOCKET.BASE_URL);
+  const dispatch = useDispatch();
+  const updateSocketInstance = useCallback(
+    socket => dispatch({ type: "UPDATE_SOCKET_INSTANCE", value: socket }),
+    [dispatch]
+  );
+  useEffect(() => {
+    updateSocketInstance(socket);
+    return () => {
+      socket.removeEventListener();
+      socket.close();
+    };
+  }, [updateSocketInstance, socket]);
   return (
     <Router>
       <Main />
